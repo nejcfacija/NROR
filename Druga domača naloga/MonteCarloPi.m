6e8f66f6dd4cfa5050eaf8dab18b73f95fae55de
nejcfacija <146846537+nejcfacija@users.noreply.github.com>
2023-11-13 20:35:49 +0100

    Add files via upload

---
+++ Druga domača naloga/MonteCarloPi.m
@@ -0,0 +1,10 @@
+(* ::Package:: *)
+
+(* ::Input:: *)
+(*MonteCarloPi[n_]:=Module[{count,x,y,distance},count=0;*)
+(*Do[x=RandomReal[];*)
+(*y=RandomReal[];*)
+(*distance=x^2+y^2;*)
+(*If[distance<=1,count++];,{n}];*)
+(*4.0 count/n]*)
+(**)
